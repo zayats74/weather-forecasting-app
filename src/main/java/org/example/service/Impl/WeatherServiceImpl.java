d52880f6b86321084c134a986289a8495cfff260
Zayats Natalia <n.zayats74@gmail.com>
2025-05-09 18:31:57 +0400

    fixing implementation of weather service with mapper

--- src/main/java/org/example/service/Impl/WeatherServiceImpl.java
+++ src/main/java/org/example/service/Impl/WeatherServiceImpl.java
@@ -1,6 +1,8 @@
 package org.example.service.Impl;
 
+import org.example.dto.WeatherResponseDTO;
 import org.example.entity.Weather;
+import org.example.mapper.WeatherMapper;
 import org.example.repository.WeatherRepository;
 import org.example.service.WeatherService;
 import org.example.utils.DateUtils;
@@ -15,45 +17,49 @@ public class WeatherServiceImpl implements WeatherService {
 
     private final WeatherRepository weatherRepository;
 
+    private final WeatherMapper weatherMapper;
+
     Random random = new Random(55);
 
-    public WeatherServiceImpl(WeatherRepository weatherRepository) {
+    public WeatherServiceImpl(WeatherRepository weatherRepository, WeatherMapper weatherMapper) {
         this.weatherRepository = weatherRepository;
+        this.weatherMapper = weatherMapper;
     }
 
-    public Weather getWeatherForecast(Random rand) {
-        return new Weather(rand);
+    public WeatherResponseDTO getWeatherForecast(Random rand) {
+        Weather w = new Weather(rand);
+        return weatherMapper.mapToResponseDTO(w);
     }
 
     @Override
-    public Weather getWeatherForecastOnToday(String city) {
+    public WeatherResponseDTO getWeatherForecastOnToday(String city) {
         LocalDate date = DateUtils.getCurrentDate();
-        weatherRepository.addWeatherForecast(city, date, this.getWeatherForecast(random));
-        return weatherRepository.getWeatherForecast(city, date);
+        weatherRepository.addWeatherForecast(city, date, weatherMapper.mapToWeather(this.getWeatherForecast(random)));
+        return weatherMapper.mapToResponseDTO(weatherRepository.getWeatherForecast(city, date));
     }
 
     @Override
-    public Weather getWeatherForecastOnTomorrow(String city) {
+    public WeatherResponseDTO getWeatherForecastOnTomorrow(String city) {
         LocalDate date = DateUtils.getTomorrowDate();
-        weatherRepository.addWeatherForecast(city, date, this.getWeatherForecast(random));
-        return weatherRepository.getWeatherForecast(city, date);
+        weatherRepository.addWeatherForecast(city, date, weatherMapper.mapToWeather(this.getWeatherForecast(random)));
+        return weatherMapper.mapToResponseDTO(weatherRepository.getWeatherForecast(city, date));
     }
 
     @Override
-    public HashMap<LocalDate, Weather> getWeatherForecastOnTenDays(String city) {
-        HashMap<LocalDate, Weather> wf = new HashMap<>();
+    public HashMap<LocalDate, WeatherResponseDTO> getWeatherForecastOnTenDays(String city) {
+        HashMap<LocalDate, WeatherResponseDTO> wf = new HashMap<>();
         LocalDate date = LocalDate.now();
         for (int i = 1; i <= 10; i++) {
-            weatherRepository.addWeatherForecast(city, date, this.getWeatherForecast(random));
-            wf.put(date, weatherRepository.getWeatherForecast(city, date));
+            weatherRepository.addWeatherForecast(city, date, weatherMapper.mapToWeather(this.getWeatherForecast(random)));
+            wf.put(date, weatherMapper.mapToResponseDTO(weatherRepository.getWeatherForecast(city, date)));
             date = date.plusDays(1);
         }
         return wf;
     }
 
     @Override
-    public Weather getWeatherForecastOnSetDay(String city, LocalDate date) {
-        weatherRepository.addWeatherForecast(city, date, this.getWeatherForecast(random));
-        return weatherRepository.getWeatherForecast(city, date);
+    public WeatherResponseDTO getWeatherForecastOnSetDay(String city, LocalDate date) {
+        weatherRepository.addWeatherForecast(city, date, weatherMapper.mapToWeather(this.getWeatherForecast(random)));
+        return weatherMapper.mapToResponseDTO(weatherRepository.getWeatherForecast(city, date));
     }
 }
