740a86d0505df1f2c29a411d1f028a2c1c52486a
Zayats Natalia <n.zayats74@gmail.com>
2025-05-20 23:53:56 +0400

    adding some tests

---
+++ src/test/java/org/example/WeatherForecastingAppTests.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WeatherForecastingAppTests {
+    @Test
+    void contextLoads() {}
+}
